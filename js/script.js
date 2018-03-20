1c44e3836c57b44f31d5ce4185ba0b37e59f8fc9
Małgorzata Skrudlik <sophie.gosia@gmail.com>
2018-03-20 18:38:32 +0100

    Add sass styling and btn js click listeners

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//script.js
+
+// New game button
+
+var newGameBtn = document.getElementById('js-newGameButton');
+
+newGameBtn.addEventListener('click', newGame); // 'click' event 
+
+// Player pick
+
+var pickRock = document.getElementById('js-playerPick_rock'),
+    pickPaper = document.getElementById('js-playerPick_paper'),
+    pickScissors = document.getElementById('js-playerPick_scissors');
+
+pickRock.addEventListener('click', function() { playerPick('rock') });
+pickPaper.addEventListener('click', function() { playerPick('paper') });
+pickScissors.addEventListener('click', function() { playerPick('scissors') });
+
+// 
